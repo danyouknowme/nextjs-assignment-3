e541cd4c73c7300c22238493e847aa8ead05c166
Thanathip Suwannakhot <dannajaeiei@hotmail.com>
2022-03-22 02:26:55 +0700

    fix navbar of each page

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,10 @@
 import type { AppProps } from 'next/app';
-import { Navbar } from '../components';
 import { GlobalStyles } from '../styles/GlobalStyles';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <GlobalStyles />
-      <Navbar />
       <Component {...pageProps} />
     </>
   );

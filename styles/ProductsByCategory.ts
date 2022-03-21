c0ff9deb719c356dfcb18eabf70f7533f380beed
Thanathip Suwannakhot <dannajaeiei@hotmail.com>
2022-03-22 02:27:14 +0700

    create product by category page

---
+++ styles/ProductsByCategory.ts
@@ -0,0 +1,20 @@
+import styled from 'styled-components';
+
+export const ProductCategoryContainer = styled.div`
+  padding: 35px 0;
+`;
+
+export const ProductCategoryMain = styled.main`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  max-width: 1350px;
+  margin: 0 auto;
+`;
+
+export const ProductCategoryCardContainer = styled.div`
+  width: 100%;
+  display: grid;
+  grid-template-columns: auto auto auto auto;
+  gap: 30px;
+`;
